068430be72778d67e9ae9fdeb49893f943806e82
JonasZ <s0558239@htw-berlin.de>
2018-10-18 17:16:37 +0200

    add comments

--- src/main/java/de/htwBerlin/runMeRunner/SelfmadeParser.java
+++ src/main/java/de/htwBerlin/runMeRunner/SelfmadeParser.java
@@ -17,18 +17,37 @@ public class SelfmadeParser {
 	public SelfmadeParser(String[] args) {
 		this.args  = args;
 		
+		// addOption() returns the resulting Options instance
+		// c is the single-character name of the option
+		// class is the multi-character name of the option
+		// true means that the argument is required after this option
+		// class name is the self-documenting description
 		options.addOption("c", "class", true, "class name");
-		options .addOption("o", "output", false, "output of report");
+		options.addOption("o", "output", false, "output of report");
 	}
 	
 	public void parse() {
+		// create the parser
 		CommandLineParser parser = new DefaultParser();
 		
-		CommandLine cmd = null;
+		CommandLine line = null;
 		try {
-			cmd = parser.parse( options, args);
-		} catch (ParseException e) {
+			// parse the command line arguments
+			// options = the specified options
+			// args = the command line arguments
+			line = parser.parse(options, args);
 			
+			if(line.hasOption("c")) {
+				String clazz = line.getOptionValue("c");
+				// else meldung , dass -c fehlt und allgemeine usage meldung
+			}
+			if(line.hasOption("o")) {
+				String output = line.getOptionValue("o");
+				// else meldung , dass -c fehlt und allgemeine usage meldung
+			}
+
+		} catch (ParseException e) {
+	        System.err.println( "Parsing failed.  Reason: " + e.getMessage() );
 		}
 	}
 
