02a5214260885ded86a2fcef78e01be3156b2258
Unknown <s0546867@htw-berlin.de>
2018-11-13 20:35:35 +0100

    "    "

---
+++ src/main/java/de/htw/ai/kbe/songsServlet/Webserver.java
@@ -0,0 +1,34 @@
+package de.htw.ai.kbe.songsServlet;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.List;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+
+public class Webserver {
+
+	//our main class
+//	
+//	// Reads a list of songs from a JSON-file into List<Song>
+//	@SuppressWarnings("unchecked")
+//	static List<OurSong> readJSONToSongs(String filename) throws FileNotFoundException, IOException {
+//	ObjectMapper objectMapper = new ObjectMapper();
+//	try (InputStream is = new BufferedInputStream(new FileInputStream(filename))) {
+//	return (List<OurSong>) objectMapper.readValue(is, new TypeReference<List<OurSong»(){});
+//	}
+		
+}
