30c3a4ce6823cd7a6af5e2b97169592725bd05d5
JonasZ <s0558239@htw-berlin.de>
2018-11-15 11:34:13 +0100

    add Database Class, add method to read/load and write/save json file

---
+++ src/main/java/de/htw/ai/kbe/songsServlet/DatabaseSongs.java
@@ -0,0 +1,99 @@
+package de.htw.ai.kbe.songsServlet;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.locks.ReentrantLock;
+
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * Klasse, um Songs Objecte in Json Format zu speichern
+ * 
+ * @author jns, camilo
+ *
+ */
+public class DatabaseSongs {
+	List<Song> songs;
+	String databaseFileName;
+	ReentrantLock dbAccessLock;
+	int songIdCounter = 0;
+
+	/**
+	 * Konstruktor
+	 * 
+	 * @param databaseFileName
+	 */
+	public DatabaseSongs(String databaseFileName) {
+		this.databaseFileName = databaseFileName;
+		assert (this.databaseFileName != null);
+		this.dbAccessLock = new ReentrantLock();
+		load();
+	}
+
+	/**
+	 * loads a list of songs from a JSON-file into List<Song>
+	 * 
+	 * @param databaseFileName
+	 */
+	@SuppressWarnings("unchecked")
+	public void load() {
+		this.dbAccessLock.lock();
+		try {
+			ObjectMapper objectMapper = new ObjectMapper();
+			try (InputStream is = new BufferedInputStream(new FileInputStream(this.databaseFileName))) {
+				songs = (List<Song>) objectMapper.readValue(is, new TypeReference<List<Song>>() {
+				});
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} finally {
+			this.dbAccessLock.unlock();
+		}
+	}
+
+	/**
+	 * saves a List<Song> to a JSON-file
+	 */
+	public void save() {
+		this.dbAccessLock.lock();
+		try {
+			ObjectMapper objectMapper = new ObjectMapper();
+			try (OutputStream os = new BufferedOutputStream(new FileOutputStream(this.databaseFileName))) {
+				objectMapper.writeValue(os, songs);
+				System.out.println("Speicherung des Inhalts der Datenbank in " + databaseFileName
+						+ " in Json Format war erfolgreich.");
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+		} finally {
+			this.dbAccessLock.unlock();
+		}
+	}
+
+	/**
+	 * return all songs in database list
+	 * @return
+	 */
+	public List<Song> getSongs() {
+		this.dbAccessLock.lock();
+		try {
+			return this.songs;
+		} finally {
+			this.dbAccessLock.unlock();
+		}
+	}
+
+}
--- src/main/java/de/htw/ai/kbe/songsServlet/SongsServlet.java
+++ src/main/java/de/htw/ai/kbe/songsServlet/SongsServlet.java
@@ -1,102 +1,80 @@
 
 package de.htw.ai.kbe.songsServlet;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.Enumeration;
-
-import javax.servlet.ServletConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletInputStream;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.annotation.WebInitParam;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.Enumeration;
 import java.util.List;
 
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
-import javax.xml.bind.Unmarshaller;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.apache.commons.io.IOUtils;
+
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+/**
+ * Klasse eines Webservices, der get und post Anfragen von Song Objekten bearbeitet
+ * @author jns, camilo
+ *
+ */
 public class SongsServlet extends HttpServlet {
-
-	private String uriToDB = null;
-	private List<OurSong> readSongs= null;
+	
+	private static final long serialVersionUID = 1L;
+	private String databaseFileName;
+	private DatabaseSongs database; 
+	// private List<Song> songs;
+	
 
 	@Override
 	public void init(ServletConfig servletConfig) throws ServletException {
-		// Beispiel: Laden eines Konfigurationsparameters aus der web.xml
-		this.uriToDB = servletConfig.getInitParameter("uriToDBComponent");
-	
-//		try {
-//		    readSongs = readJSONToSongs("/Users/camiloocampo/Desktop/database/songs.json");
-//			readSongs.forEach(s -> {
-//				System.out.println(s.getArtist());
-//			});
-//			writeSongsToJSON(readSongs, "outJacksonSongs.json");
-//		} catch (Exception e) { // Was stimmt hier nicht?
-//		}
+		this.databaseFileName = servletConfig.getInitParameter("uriToDatabaseFile");
+		this.database = new DatabaseSongs(databaseFileName);
 	}
 
-	// Reads a list of songs from a JSON-file into List<Song>
-	@SuppressWarnings("unchecked")
-	static List<OurSong> readJSONToSongs(String filename) throws FileNotFoundException, IOException {
-		ObjectMapper objectMapper = new ObjectMapper();
-		try (InputStream is = new BufferedInputStream(new FileInputStream(filename))) {
-			return (List<OurSong>) objectMapper.readValue(is, new TypeReference<List<OurSong>>() {
-			});
-		}
-	}
-
-	// Write a List<Song> to a JSON-file
-	static void writeSongsToJSON(List<OurSong> songs, String filename) throws FileNotFoundException, IOException {
-		ObjectMapper objectMapper = new ObjectMapper();
-		try (OutputStream os = new BufferedOutputStream(new FileOutputStream(filename))) {
-			objectMapper.writeValue(os, songs);
+	@Override
+	public void destroy() {
+		System.out.print("Methode destroy() wurde aufgerufen, Servlet wird vom Container entsorgt.");
+		
+		if(database != null) {
+			this.database.save();		
 		}
 	}
 
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		if (database == null) {
+			response.sendError(500);
+			// response.setStatus(500);
+			return;
+		}
+		// Test, wird in $TOM_HOME/logs/catalina.out geprintet
+		System.out.println("GET request: " + request.getPathInfo() + " / " + request.getQueryString());
 		
-		// alle Parameter (keys)
+		// Test: drucke die IDs, Artists und Title aller songs in log file
+		database.getSongs().forEach(
+				s -> {System.out.println(s.getId() + ". Aartist: " + s.getArtist() + ", Title: " + s.getTitle() + "\n");});
+		
+		
+		// Test: drucke alle Parameter (keys) der Request in log file 
 		Enumeration<String> paramNames = request.getParameterNames();
-
-		String responseStr = "";
-		String param = "";
 		while (paramNames.hasMoreElements()) {
-			param = paramNames.nextElement();
-			responseStr = responseStr + param + "=" 
-			+ request.getParameter(param) + "\n";
-		}
-		response.setContentType("text/plain");
-		try (PrintWriter out = response.getWriter()) {
-			responseStr += "\n Your request will be sent to " + uriToDB;
-			out.println(responseStr);
+			System.out.println("Parameter: " + paramNames.nextElement());
 		}
 		
-		
-		//try (PrintWriter out = response.getWriter()) {
-		//	responseStr += "\n erster song. " + readSongs.get(0).toString();
-		//	out.println(responseStr);
-		//}
-		
-		
-		
+		// doGet verlief erfolgreich
+		response.setStatus(200);
 	}
 
 	@Override
@@ -107,10 +85,6 @@ public class SongsServlet extends HttpServlet {
 		try (PrintWriter out = response.getWriter()) {
 			out.println(new String(inBytes));
 		}
-	}
-
-	protected String getUriToDB() {
-		return this.uriToDB;
 	}
 
 }
